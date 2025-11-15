07bba409e48093f483e611f092494ba010dfca9f
Yuki Matsushita <yuki_a_1208@yahoo.co.jp>
2025-11-15 17:00:10 +0900

    ポンプが水を吸いました

--- source/main/main.ino
+++ source/main/main.ino
@@ -1,22 +1,41 @@
 
 #include <M5CoreS3.h>
 
-void setup() {
-  // CoreS3 の初期化
-  M5.begin();
+#define INPUT_PIN G1
+#define PUMP_PIN  G2
 
-  // 画面を黒でクリア
-  M5.Lcd.fillScreen(BLACK);
+bool flag = true;
+int rawADC;
 
-  // 文字色とサイズを設定
-  M5.Lcd.setTextColor(WHITE);
-  M5.Lcd.setTextSize(3);
-
-  // 文字表示
-  M5.Lcd.setCursor(20, 50);
-  M5.Lcd.println("Hello CoreS3!");
+void setup()
+{
+    M5.begin();
+    Serial.begin(9600);
+    M5.Lcd.setTextColor(GREEN);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.setTextDatum(TC_DATUM);
+    M5.Lcd.drawString("Watering TEST", 160, 20, 1);
+    M5.Lcd.drawString("ON/OFF PUMP", 160, 200, 1);
+    pinMode(INPUT_PIN, INPUT);
+    pinMode(PUMP_PIN, OUTPUT);
+    pinMode(25, OUTPUT);
+    digitalWrite(25, 0);
 }
 
-void loop() {
-  // 画面表示だけなのでループ内は何もしない
+char info[30];
+
+void loop()
+{
+    rawADC = analogRead(INPUT_PIN);
+    M5.Lcd.fillRect(80, 100, 240, 50, BLACK);
+    M5.Lcd.setCursor(80, 100);
+    M5.Lcd.print("ADC: " + String(rawADC));
+    Serial.print("Watering ADC value: ");
+    Serial.println(rawADC);
+    if (M5.BtnB.wasPressed()) {
+        digitalWrite(PUMP_PIN, flag);
+        flag = !flag;
+    }
+    M5.update();
+    delay(100);
 }
